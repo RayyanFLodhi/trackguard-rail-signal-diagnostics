a6fd244fce45b60ce41b1170f777c6f7ccd24cef
RayyanFLodhi <lodhi.rayyan@gmail.com>
2026-03-13 01:17:31 -0400

    updating perl script

--- automation/perl/parse_logs.pl
+++ automation/perl/parse_logs.pl
@@ -1,33 +1,53 @@
 use strict;
 use warnings;
 
-my $file = "logs/exports/control_center_export.csv";
+my $file = "../../logs/exports/simulator_events.csv";
 
 open(my $fh, "<", $file) or die "Could not open file '$file': $!";
 
-my $header = <$fh>;
+my $header = <$fh>;  # skip header
 
 my $total = 0;
+my $healthy = 0;
 my $faulted = 0;
-
-print "TrackGuard Device Export Summary\n";
-print "--------------------------------\n";
+my %fault_counts;
+my @faulted_devices;
 
 while (my $line = <$fh>) {
     chomp $line;
+    next if $line =~ /^\s*$/;
+
     my ($device_id, $type, $state, $fault) = split /,/, $line;
 
     $total++;
 
-    if ($fault ne "NONE") {
+    if ($fault eq "NONE") {
+        $healthy++;
+    } else {
         $faulted++;
-        print "Fault detected: $device_id ($type) -> $fault\n";
+        $fault_counts{$fault}++;
+        push @faulted_devices, "$device_id ($type) -> $fault";
     }
 }
 
 close($fh);
 
-print "--------------------------------\n";
+print "TrackGuard Device Summary\n";
+print "-------------------------\n";
 print "Total devices: $total\n";
-print "Devices with faults: $faulted\n";
-print "Devices healthy: " . ($total - $faulted) . "\n";
+print "Healthy devices: $healthy\n";
+print "Faulted devices: $faulted\n\n";
+
+print "Fault breakdown:\n";
+foreach my $fault (sort keys %fault_counts) {
+    print "$fault: $fault_counts{$fault}\n";
+}
+
+print "\nFaulted devices:\n";
+if (@faulted_devices) {
+    foreach my $entry (@faulted_devices) {
+        print "$entry\n";
+    }
+} else {
+    print "None\n";
+}
